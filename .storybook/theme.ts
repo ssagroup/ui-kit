926d3f700135dee570dba14e17a2a7fc1008eb82
Artur Kashaiev <artur.kashaev@ssa.group>
2023-08-11 12:52:34 +0300

    Replace the default logo with a branded logo draft

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'SSA UI Kit',
+  brandUrl: 'https://www.ssa.group/',
+  brandImage:
+    'https://firebasestorage.googleapis.com/v0/b/admin-themes.appspot.com/o/logo%2Fssa_ui_kit_logo_left.png?alt=media&token=491023a1-1c70-4a17-a48a-d5db81ad9fb4',
+});
