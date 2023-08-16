8a2c3987c880eca56b81db0926596aca87f9203d
Kostiantyn <kostiantyn.petrenko@ssa.group>
2023-08-16 16:37:45 +0300

    GH-20 Configuring

--- packages/widgets/lostpixel.config.ts
+++ packages/widgets/lostpixel.config.ts
@@ -4,7 +4,7 @@ export const config: Partial<CustomProjectConfig> = {
   storybookShots: {
     storybookUrl: './storybook-static',
   },
-
-  lostPixelProjectId: 'clldjub5z3vtpit0ecdeldbxe',
+  // @cspell:disable-next-line
+  lostPixelProjectId: 'clldrvzro4dkcna0ez80h1y2h',
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
