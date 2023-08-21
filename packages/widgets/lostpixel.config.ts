394550adce774a564952fe4d8f3b3a3d44b51e00
Kostiantyn <kostiantyn.petrenko@ssa.group>
2023-08-21 11:56:13 +0300

    GH-20 Breakpoints added

--- packages/widgets/lostpixel.config.ts
+++ packages/widgets/lostpixel.config.ts
@@ -1,8 +1,13 @@
 import { CustomProjectConfig } from 'lost-pixel';
 
-export const config: Partial<CustomProjectConfig> = {
+export const config: Partial<CustomProjectConfig> & {
+  storybookShots: {
+    breakpoints: number[];
+  };
+} = {
   storybookShots: {
     storybookUrl: './packages/widgets/storybook-static',
+    breakpoints: [1024, 1440],
   },
   // @cspell:disable-next-line
   lostPixelProjectId: 'clldrvzro4dkcna0ez80h1y2h',
