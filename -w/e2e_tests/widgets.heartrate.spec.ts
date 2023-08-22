917e627d603986dd004d50a9f9073b27243047bc
Kostiantyn <kostiantyn.petrenko@ssa.group>
2023-08-22 18:35:37 +0300

    Configuring

--- -w/e2e_tests/widgets.heartrate.spec.ts
+++ -w/e2e_tests/widgets.heartrate.spec.ts
@@ -1,18 +1,28 @@
 import { test, expect } from '@playwright/test';
 
+test('Widgets: HeartRate item should be present in the sidebar', async ({
+  page,
+}) => {
+  await page.goto('http://172.17.0.1:6007');
+  await page.waitForTimeout(7000);
+  await expect(page.getByText('HeartRate')).toBeVisible();
+});
+
 test('Widgets: HeartRate - Hint is shown', async ({ page }) => {
   // await page.goto(
   //   'https://ui-kit-fitness.web.app/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
   // );
-  await page.goto(
-    // 'http://localhost:6007/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
-    // 'http://172.17.0.1:6007/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
-    // 'http://172.17.0.1:8080/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
-    'http://172.17.0.1:6007',
-    // 'http://localhost:6007',
-  );
-  await page.waitForTimeout(7000);
-  await expect(page.getByText('HeartRate')).toBeVisible();
+  // await page.goto(
+  //   // 'http://localhost:6007/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
+  //   // 'http://172.17.0.1:6007/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
+  //   // 'http://172.17.0.1:8080/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
+  //   'http://172.17.0.1:6007',
+  //   // 'http://localhost:6007',
+  // );
+  // await page.waitForTimeout(7000);
+  await page.getByText('HeartRate').click();
+  await page.getByText('Default').click();
+  await expect(page.getByText('Heart Rate')).toBeVisible();
   // await page.mouse.move(64, 243);
   // await page.mouse.click(64, 243);
 
