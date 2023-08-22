811e65f4f1375625e2cb639615b9c4ed33b4d4f4
Kostiantyn <kostiantyn.petrenko@ssa.group>
2023-08-22 19:15:08 +0300

    Configuring

--- -w/e2e_tests/widgets.heartrate.spec.ts
+++ -w/e2e_tests/widgets.heartrate.spec.ts
@@ -4,7 +4,7 @@ test.describe.configure({ mode: 'serial' });
 
 let page: Page;
 
-test.beforeAll(async ({ page, browser }) => {
+test.beforeAll(async ({ browser }) => {
   page = await browser.newPage();
 });
 
@@ -12,43 +12,26 @@ test.afterAll(async () => {
   await page.close();
 });
 
-test('Widgets: HeartRate item should be present in the sidebar', async ({
-  page,
-}) => {
+test('Widgets: HeartRate item should be present in the sidebar', async () => {
   await page.goto('http://172.17.0.1:6007');
-  await page.waitForTimeout(7000);
+  // await page.goto('http://localhost:6007');
+  await page.waitForTimeout(1000);
+  await page.getByRole('button', { name: 'Activity' }).click();
   await expect(page.getByText('HeartRate')).toBeVisible();
 });
 
-test('Widgets: HeartRate - Hint is shown', async ({ page }) => {
-  // await page.goto(
-  //   'https://ui-kit-fitness.web.app/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
-  // );
-  // await page.goto(
-  //   // 'http://localhost:6007/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
-  //   // 'http://172.17.0.1:6007/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
-  //   // 'http://172.17.0.1:8080/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
-  //   'http://172.17.0.1:6007',
-  //   // 'http://localhost:6007',
-  // );
-  // await page.waitForTimeout(7000);
-  await page.getByText('HeartRate').click();
-  await page.getByText('Default').click();
-  await expect(page.getByText('Heart Rate')).toBeVisible();
-  // await page.mouse.move(64, 243);
-  // await page.mouse.click(64, 243);
+test('Widgets: HeartRate - Hint is shown', async () => {
+  await page.getByRole('button', { name: 'HeartRate' }).click();
+  await page.getByRole('link', { name: 'Default' }).click();
+  const page2Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'Open canvas in new tab' }).click();
+  const page2 = await page2Promise;
+  await page2.waitForTimeout(7000);
+  await page2.mouse.move(546, 82);
+  await expect(page2.getByText('04:03 AM - 60 bpm')).toBeVisible();
 
-  // // await page.mouse.move(88, 290);
-  // await page.waitForTimeout(100);
-  // await page.mouse.click(88, 290);
-  // await page.waitForTimeout(100);
-  // await page.mouse.move(481, 87);
-  // await page.waitForTimeout(100);
-
-  await page.screenshot({
+  await page2.screenshot({
     path: '../lost-pixel/widgets/HeartRateHint.png',
     fullPage: true,
   });
-
-  // await expect(page.getByText('08:03 PM - 120 bpm')).toBeVisible();
 });
