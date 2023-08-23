0550cb39b6610cbfb494ae21bc25b9b99bdc01c8
Kostiantyn <kostiantyn.petrenko@ssa.group>
2023-08-23 15:54:20 +0300

    Debugging

--- -w/e2e_tests/widgets.heartrate.spec.ts
+++ -w/e2e_tests/widgets.heartrate.spec.ts
@@ -27,21 +27,20 @@ test('Widgets: HeartRate item should be present in the sidebar', async () => {
   });
 });
 
-// test('Widgets: HeartRate - Hint is shown', async () => {
-//   await page.waitForTimeout(5000);
-//   await page.mouse.move(663, 350);
-//   await page.mouse.move(660, 350, {
-//     steps: 5,
-//   });
-//   await page.waitForTimeout(500);
-//   await expect(
-//     page
-//       .frameLocator('iframe[title="storybook-preview-iframe"]')
-//       .getByText('03:03 AM - 80 bpm'),
-//   ).toBeVisible();
-
-//   await page.screenshot({
-//     path: '../../lost-pixel/widgets/HeartRateHint.png',
-//     fullPage: true,
-//   });
-// });
+test('Widgets: HeartRate - Hint is shown', async () => {
+  await page.waitForTimeout(5000);
+  await page.mouse.move(663, 350);
+  await page.mouse.move(660, 350, {
+    steps: 5,
+  });
+  await page.waitForTimeout(500);
+  await page.screenshot({
+    path: '../lost-pixel/widgets/HeartRateHint.png',
+    fullPage: true,
+  });
+  // await expect(
+  //   page
+  //     .frameLocator('iframe[title="storybook-preview-iframe"]')
+  //     .getByText('03:03 AM - 80 bpm'),
+  // ).toBeVisible();
+});
