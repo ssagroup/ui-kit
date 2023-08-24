7c7a9ad957e90f8a3fab8ea9c7a59c4bd62bc7e2
Kostiantyn <kostiantyn.petrenko@ssa.group>
2023-08-24 15:56:28 +0300

    Configuring

--- -w/e2e_tests/widgets.heartrate.spec.ts
+++ -w/e2e_tests/widgets.heartrate.spec.ts
@@ -18,24 +18,7 @@ test('Widgets: HeartRate item should be present in the sidebar', async () => {
   await page.goto(
     'http://localhost:6007/iframe.html?args=&id=widgets-heartrate--default&viewMode=story',
   );
-
   await expect(page.getByRole('heading', { name: 'Heart Rate' })).toBeVisible();
-  await page.setViewportSize({ width: 1920, height: 1080 });
-  await page.screenshot({
-    path: `${WIDGETS_CUSTOM_SHOTS_PATH}HeartRateDefault__[w1920px].png`,
-  });
-  await page.setViewportSize({ width: 1440, height: 900 });
-  await page.screenshot({
-    path: `${WIDGETS_CUSTOM_SHOTS_PATH}HeartRateDefault__[w1440px].png`,
-  });
-  await page.setViewportSize({ width: 900, height: 900 });
-  await page.screenshot({
-    path: `${WIDGETS_CUSTOM_SHOTS_PATH}HeartRateDefault__[w900px].png`,
-  });
-  await page.setViewportSize({ width: 390, height: 390 });
-  await page.screenshot({
-    path: `${WIDGETS_CUSTOM_SHOTS_PATH}HeartRateDefault__[w390px].png`,
-  });
 });
 
 test('Widgets: HeartRate - Hint is shown', async () => {
@@ -45,20 +28,22 @@ test('Widgets: HeartRate - Hint is shown', async () => {
   });
   await page.waitForTimeout(500);
   // await expect(page.getByText('09:03 PM - 75 bpm')).toBeVisible();
+
+  const SCREENSHOT_PREFIX = `${WIDGETS_CUSTOM_SHOTS_PATH}widgets-heart-rate--hint-opened__`;
   await page.setViewportSize({ width: 1920, height: 1080 });
   await page.screenshot({
-    path: `${WIDGETS_CUSTOM_SHOTS_PATH}HeartRateHint__[w1920px].png`,
+    path: `${SCREENSHOT_PREFIX}[w1920px].png`,
   });
   await page.setViewportSize({ width: 1440, height: 900 });
   await page.screenshot({
-    path: `${WIDGETS_CUSTOM_SHOTS_PATH}HeartRateHint__[w1440px].png`,
+    path: `${SCREENSHOT_PREFIX}[w1440px].png`,
   });
   await page.setViewportSize({ width: 900, height: 900 });
   await page.screenshot({
-    path: `${WIDGETS_CUSTOM_SHOTS_PATH}HeartRateHint__[w900px].png`,
+    path: `${SCREENSHOT_PREFIX}[w900px].png`,
   });
   await page.setViewportSize({ width: 390, height: 390 });
   await page.screenshot({
-    path: `${WIDGETS_CUSTOM_SHOTS_PATH}HeartRateHint__[w390px].png`,
+    path: `${SCREENSHOT_PREFIX}[w390px].png`,
   });
 });
