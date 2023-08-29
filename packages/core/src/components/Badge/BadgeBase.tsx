07bd7b25ace7e31c10bc1f2f9659b1b7ec39ba5d
react-developer-ssa-group <141148643+react-developer-ssa-group@users.noreply.github.com>
2023-08-29 18:22:44 +0300

    GH-40 Fixed badge component height (#42)
    [Fix <Badge /> component
    height](https://github.com/ssagroup/ui-kit/issues/40)
    Close #40

--- packages/core/src/components/Badge/BadgeBase.tsx
+++ packages/core/src/components/Badge/BadgeBase.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 
-const BadgeBase = styled.span`
+const BadgeBase = styled.div`
+  display: inline-block;
+
   font-family: Manrope, sans-serif;
   font-style: normal;
   font-weight: 500;
@@ -9,6 +11,8 @@ const BadgeBase = styled.span`
 
   text-align: center;
   color: #fff;
+
+  white-space: pre;
 `;
 
 export default BadgeBase;
