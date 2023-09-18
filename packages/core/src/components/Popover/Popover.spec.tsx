9475dc76e75822a0d2dc95bfe3c471556ea13953
Kostiantyn <kostiantyn.petrenko@ssa.group>
2023-09-18 15:15:51 +0300

    GH-62 Interfaces. Library updated

---
+++ packages/core/src/components/Popover/Popover.spec.tsx
@@ -0,0 +1,17 @@
+import { fireEvent } from '@testing-library/dom';
+import { StoryComponent } from './stories/StoryComponent';
+
+describe('Popover', () => {
+  it('Shows up when the trigger is clicked', () => {
+    const { getByText, getByRole, queryByText } = render(<StoryComponent />);
+
+    const buttonEl = getByRole('button');
+    let closeButtonEl = queryByText('Close');
+    expect(closeButtonEl).not.toBeInTheDocument();
+    fireEvent.click(buttonEl);
+    closeButtonEl = getByText('Close');
+    expect(closeButtonEl).toBeInTheDocument();
+    fireEvent.click(closeButtonEl);
+    expect(queryByText('Close')).not.toBeInTheDocument();
+  });
+});
