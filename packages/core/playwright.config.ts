7ac92f6418915f8216de61c4a0a13f7d5aef38b7
Kostiantyn <kostiantyn.petrenko@ssa.group>
2023-09-19 10:19:56 +0300

    Popover playwright test and screenshot

---
+++ packages/core/playwright.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from '@playwright/test';
+import { createConfig } from '../../playwright.base.config';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig(createConfig());
