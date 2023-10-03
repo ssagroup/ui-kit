7836953b0f294b17a93d036f9e14485ae3a630b3
Artur Kashaiev <artur.kashaev@ssa.group>
2023-10-03 16:19:15 +0300

    WIP: remove timeouts from Popover's e2e tests

--- packages/core/src/components/Popover/Popover.e2e.ts
+++ packages/core/src/components/Popover/Popover.e2e.ts
@@ -5,28 +5,30 @@ test.describe.configure({ mode: 'serial' });
 
 let page: Page;
 
-test.beforeAll(async ({ browser }) => {
+test.beforeEach(async ({ browser }) => {
   page = await browser.newPage();
 });
 
-test.afterAll(async () => {
+test.afterEach(async () => {
   await page.close();
 });
 
 const CUSTOM_SHOTS_PATH = './custom-shots/';
 
-test('Core: Popover should be opened correctly', async () => {
-  await page.goto(
+const gotoPage = () => {
+  return page.goto(
     'http://localhost:6006/iframe.html?args=&globals=&id=components-popover--default&viewMode=story',
   );
+};
+
+test('Core: Popover should be opened correctly', async () => {
+  await gotoPage();
   await expect(page.getByText('Trigger')).toBeVisible();
 });
 
 test('Core: Popover - Hint is shown', async () => {
-  await page.waitForTimeout(500);
+  await gotoPage();
   await page.setViewportSize(SCREEN_SIZES[1920]);
-  await page.waitForTimeout(100);
-  await page.mouse.wheel(0, 1400);
   await page.getByText('Trigger').click();
 
   const SCREENSHOT_PREFIX = `${CUSTOM_SHOTS_PATH}components-popover--hint-opened__`;
