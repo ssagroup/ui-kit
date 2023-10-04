c2185897ae7f27930e12c30fe1745a9a8a1bbafb
Artur Kashaiev <artur.kashaev@ssa.group>
2023-10-04 13:26:43 +0300

    Add base lostpixel config, increase delay before screenshot

--- packages/core/lostpixel.config.ts
+++ packages/core/lostpixel.config.ts
@@ -1,17 +1,18 @@
+import { baseConfig } from '../../lostpixel.base.config';
+import { SCREEN_SIZES } from './src/consts';
 import { CustomProjectConfig } from 'lost-pixel';
+
+const breakpoints = Object.keys(SCREEN_SIZES).map(Number);
 
 export const config: Partial<CustomProjectConfig> & {
   storybookShots: {
     breakpoints: number[];
   };
-} = {
+} = Object.assign({}, baseConfig, {
   storybookShots: {
     storybookUrl: './packages/core/storybook-static',
-    breakpoints: [390, 900, 1440, 1920],
+    breakpoints,
   },
   // @cspell:disable-next-line
   lostPixelProjectId: 'clldrwk2t4dkena0e4nu2rikp',
-  apiKey: process.env.LOST_PIXEL_API_KEY,
-  shotConcurrency: 10,
-  compareConcurrency: 20,
-};
+});
--- packages/widgets/lostpixel.config.ts
+++ packages/widgets/lostpixel.config.ts
@@ -1,20 +1,21 @@
+import { baseConfig } from '../../lostpixel.base.config';
+import { SCREEN_SIZES } from './src/consts';
 import { CustomProjectConfig } from 'lost-pixel';
+
+const breakpoints = Object.keys(SCREEN_SIZES).map(Number);
 
 export const config: Partial<CustomProjectConfig> & {
   storybookShots: {
     breakpoints: number[];
   };
-} = {
+} = Object.assign({}, baseConfig, {
   storybookShots: {
     storybookUrl: './packages/widgets/storybook-static',
-    breakpoints: [390, 900, 1440, 1920],
+    breakpoints,
   },
   customShots: {
     currentShotsPath: './packages/widgets/custom-shots',
   },
   // @cspell:disable-next-line
   lostPixelProjectId: 'clldrvzro4dkcna0ez80h1y2h',
-  apiKey: process.env.LOST_PIXEL_API_KEY,
-  shotConcurrency: 10,
-  compareConcurrency: 20,
-};
+});
