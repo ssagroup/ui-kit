4e27290bfa1e2c23e06575676d712f6faa8fa894
Artur Kashaiev <artur.kashaev@ssa.group>
2023-10-04 15:46:56 +0300

    Split NavBar's e2e test into multiple ones

--- packages/widgets/src/components/NavBar/NavBar.e2e.ts
+++ packages/widgets/src/components/NavBar/NavBar.e2e.ts
@@ -5,39 +5,51 @@ test.describe.configure({ mode: 'serial' });
 
 let page: Page;
 
-test.beforeAll(async ({ browser }) => {
+test.beforeEach(async ({ browser }) => {
   page = await browser.newPage();
 });
 
-test.afterAll(async () => {
+test.afterEach(async () => {
   await page.close();
 });
 
 const WIDGETS_CUSTOM_SHOTS_PATH = './custom-shots/';
 const SCREENSHOT_PREFIX = `${WIDGETS_CUSTOM_SHOTS_PATH}widgets-navbar-opened__`;
 
-test('Widgets: NavBar should be visible after icon clicking', async () => {
-  await page.setViewportSize(SCREEN_SIZES[1920]);
-  await page.goto(
+const gotoPage = () => {
+  return page.goto(
     'http://localhost:6007/iframe.html?id=widgets-navbar--default&viewMode=story',
   );
+};
 
+test('[1920] Widgets: NavBar should be visible', async () => {
+  await page.setViewportSize(SCREEN_SIZES[1920]);
+  await gotoPage();
   await page.screenshot({
     path: `${SCREENSHOT_PREFIX}[w1920px].png`,
   });
+});
 
+test('[1440] Widgets: NavBar should be visible', async () => {
   await page.setViewportSize(SCREEN_SIZES[1440]);
+  await gotoPage();
   await page.screenshot({
     path: `${SCREENSHOT_PREFIX}[w1440px].png`,
   });
+});
 
+test('[900] Widgets: NavBar should be visible after icon clicking', async () => {
   await page.setViewportSize(SCREEN_SIZES[900]);
+  await gotoPage();
   await page.locator('#storybook-root div').nth(1).click();
   await page.screenshot({
     path: `${SCREENSHOT_PREFIX}[w900px].png`,
   });
+});
 
+test('[390] Widgets: NavBar should be visible after icon clicking', async () => {
   await page.setViewportSize(SCREEN_SIZES[390]);
+  await gotoPage();
   await page.screenshot({
     path: `${SCREENSHOT_PREFIX}[w390px].png`,
   });
