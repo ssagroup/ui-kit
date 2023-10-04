07c163c0486cb81ce22d54bcec926afafdad4837
Artur Kashaiev <artur.kashaev@ssa.group>
2023-10-04 12:42:36 +0300

    Refactor TableFilter's e2e tests

--- packages/widgets/src/components/TableFilters/TableFilters.e2e.ts
+++ packages/widgets/src/components/TableFilters/TableFilters.e2e.ts
@@ -5,29 +5,33 @@ test.describe.configure({ mode: 'serial' });
 
 let page: Page;
 
-test.beforeAll(async ({ browser }) => {
+test.beforeEach(async ({ browser }) => {
   page = await browser.newPage();
 });
 
-test.afterAll(async () => {
+test.afterEach(async () => {
   await page.close();
 });
 
 const WIDGETS_CUSTOM_SHOTS_PATH = './custom-shots/';
+const SCREENSHOT_PREFIX = `${WIDGETS_CUSTOM_SHOTS_PATH}widgets-table-filters--opened__`;
 
-test('Widgets: TableFilters should be opened correctly', async () => {
-  await page.goto(
+const gotoPage = () => {
+  return page.goto(
     'http://localhost:6007/iframe.html?args=&id=widgets-tablefilters--default&viewMode=story',
   );
+};
+
+test('Widgets: TableFilters should be opened correctly', async () => {
+  await gotoPage();
   await expect(page.getByText('More')).toBeVisible();
 });
 
 test('Widgets: TableFilters - should be shown', async () => {
   await page.setViewportSize(SCREEN_SIZES[1920]);
-  await page.waitForTimeout(100);
+  await gotoPage();
   await page.getByText('More').click();
 
-  const SCREENSHOT_PREFIX = `${WIDGETS_CUSTOM_SHOTS_PATH}widgets-table-filters--opened__`;
   await page.screenshot({
     path: `${SCREENSHOT_PREFIX}[w1920px].png`,
   });
