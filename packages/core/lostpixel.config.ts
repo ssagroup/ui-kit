d6e2afae95834d75b0ba7a062888e99d77007bdf
Artur Kashaiev <artur.kashaev@ssa.group>
2023-10-04 19:16:57 +0300

    Fix mask definition in lostpixel config, refactor config definition

--- packages/core/lostpixel.config.ts
+++ packages/core/lostpixel.config.ts
@@ -4,17 +4,23 @@ import { CustomProjectConfig } from 'lost-pixel';
 
 const breakpoints = Object.keys(SCREEN_SIZES).map(Number);
 
-export const config: Partial<CustomProjectConfig> & {
+const coreConfig: Partial<CustomProjectConfig> & {
   storybookShots: {
     breakpoints: number[];
   };
-} = Object.assign({}, baseConfig, {
+} = {
   storybookShots: {
     storybookUrl: './packages/core/storybook-static',
     breakpoints,
     // https://docs.lost-pixel.com/user-docs/api-reference/mask
-    mask: ['.lostpixel-ignore'],
+    mask: [{ selector: '.lostpixel-ignore' }],
   },
   // @cspell:disable-next-line
   lostPixelProjectId: 'clldrwk2t4dkena0e4nu2rikp',
-});
+};
+
+export const config: Partial<CustomProjectConfig> & {
+  storybookShots: {
+    breakpoints: number[];
+  };
+} = Object.assign({}, baseConfig, coreConfig);
--- packages/widgets/lostpixel.config.ts
+++ packages/widgets/lostpixel.config.ts
@@ -4,11 +4,11 @@ import { CustomProjectConfig } from 'lost-pixel';
 
 const breakpoints = Object.keys(SCREEN_SIZES).map(Number);
 
-export const config: Partial<CustomProjectConfig> & {
+const widgetsConfig: Partial<CustomProjectConfig> & {
   storybookShots: {
     breakpoints: number[];
   };
-} = Object.assign({}, baseConfig, {
+} = {
   storybookShots: {
     storybookUrl: './packages/widgets/storybook-static',
     breakpoints,
@@ -18,4 +18,10 @@ export const config: Partial<CustomProjectConfig> & {
   },
   // @cspell:disable-next-line
   lostPixelProjectId: 'clldrvzro4dkcna0ez80h1y2h',
-});
+};
+
+export const config: Partial<CustomProjectConfig> & {
+  storybookShots: {
+    breakpoints: number[];
+  };
+} = Object.assign({}, baseConfig, widgetsConfig);
