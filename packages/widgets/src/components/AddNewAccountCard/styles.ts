85a3e09dc72ff0d2b46dbd150ae0f0865c004707
Vitalii Ilchenko <vitalii.ilchenko@ssa.group>
2023-10-27 19:18:58 +0300

    WIP

--- packages/widgets/src/components/AddNewAccountCard/styles.ts
+++ packages/widgets/src/components/AddNewAccountCard/styles.ts
@@ -1,7 +1,6 @@
-import { css } from '@emotion/react';
-import { mainTheme } from '@ssa-ui-kit/core';
+import { Theme, css } from '@emotion/react';
 
-export const AccountCard = css`
+export const AccountCard = (theme: Theme) => css`
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -9,22 +8,22 @@ export const AccountCard = css`
   gap: 6px;
   width: 100%;
   padding: 21px 10px;
-  color: ${mainTheme.colors.blueRoyal};
+  color: ${theme.colors.blueRoyal};
   font-size: 14px;
   font-weight: 500;
   line-height: 18px;
   letter-spacing: 0;
   border-radius: 20px;
   padding: 21px 0;
-  background: ${mainTheme.colors.greyLighter};
-  border: 1px solid ${mainTheme.colors.white};
-  box-shadow: 0px 10px 40px 0px ${mainTheme.colors.greyShadow};
+  background: ${theme.colors.greyLighter};
+  border: 1px solid ${theme.colors.white};
+  box-shadow: 0px 10px 40px 0px ${theme.colors.greyShadow};
   transition: 0.3s;
   cursor: pointer;
 
   &:hover {
-    border: 1px solid ${mainTheme.colors.white};
-    background: ${mainTheme.colors.greyLighter};
-    box-shadow: 5px 5px 20px 0px ${mainTheme.colors.black25};
+    border: 1px solid ${theme.colors.white};
+    background: ${theme.colors.greyLighter};
+    box-shadow: 5px 5px 20px 0px ${theme.colors.black25};
   }
 `;
