117bdfe7be458e01d0f71d64a658a9bf57f75ac1
Artur Kashaiev <artur.kashaev@ssa.group>
2023-11-16 16:46:37 +0200

    Change interface to type

--- packages/core/src/components/Modal/types.ts
+++ packages/core/src/components/Modal/types.ts
@@ -1,5 +1,4 @@
 import type { ModalDialogProps } from '@components/ModalDialog';
 export type ModalContextProps = [boolean, (isOpen: boolean) => void];
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ModalProps extends Partial<ModalDialogProps> {}
+export type ModalProps = Partial<ModalDialogProps>;
