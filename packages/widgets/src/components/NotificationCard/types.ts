1e717dfd329379d8068dd0af90166c4a4834ab59
Artur Kashaiev <artur.kashaev@ssa.group>
2023-11-22 12:21:52 +0200

    NotificationCard: ref & custom styles

--- packages/widgets/src/components/NotificationCard/types.ts
+++ packages/widgets/src/components/NotificationCard/types.ts
@@ -1,9 +1,12 @@
+export type NotificationType = 'Informational' | 'Warning' | 'Error';
+
 export interface NotificationCardProps {
   title: string;
   text: string;
   isRead: boolean;
-  type: 'Informational' | 'Warning' | 'Error';
+  type: NotificationType;
   time: string | number;
   children?: React.ReactNode;
   onClick?: () => void;
+  className?: string;
 }
