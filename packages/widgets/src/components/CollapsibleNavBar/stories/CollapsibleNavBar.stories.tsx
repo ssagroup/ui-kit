ed3835278d204344e447610bd3d09b5e5dbf0d47
Artur Kashaiev <artur.kashaev@ssa.group>
2023-11-29 13:23:14 +0200

    Improve story layout

--- packages/widgets/src/components/CollapsibleNavBar/stories/CollapsibleNavBar.stories.tsx
+++ packages/widgets/src/components/CollapsibleNavBar/stories/CollapsibleNavBar.stories.tsx
@@ -1,5 +1,4 @@
 import { Fragment } from 'react';
-import styled from '@emotion/styled';
 import { Routes, Route, MemoryRouter } from 'react-router-dom';
 import { Meta } from '@storybook/react';
 import { Title, Description, Source } from '@storybook/addon-docs';
@@ -8,33 +7,9 @@ import { DecoratorFunction } from '@storybook/types';
 import { CollapsibleNavBar } from '../CollapsibleNavBar';
 import { ITEMS } from './consts';
 import { Logo } from './Logo';
+import { Layout } from './Layout';
 
 type Args = Parameters<typeof CollapsibleNavBar>[0];
-
-const Main = styled.main`
-  background: linear-gradient(
-      143deg,
-      #e7ebf1 -4.16%,
-      #d7d9dd 39.37%,
-      #cccdd2 52.66%,
-      #e1e4ea 87.68%
-    ),
-    #f8f9fb;
-  width: 100%;
-  padding: 60px 0 0 15px;
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    padding: 0 21px 0 35px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.lg} {
-    padding: 0 49px 0 36px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.xlg} {
-    padding: 0 60px 0 62px;
-  }
-`;
 
 const reactRouterDecorator: DecoratorFunction<
   {
@@ -68,29 +43,7 @@ export default {
       ),
     },
   },
-  decorators: [
-    reactRouterDecorator,
-    (Story) => (
-      <div
-        css={{
-          display: 'flex',
-          minHeight: '100vh',
-          position: 'relative',
-        }}>
-        {Story()}
-        <Main>
-          {new Array(200).fill(1).map((_, index) => (
-            <button
-              key={index}
-              onClick={() => alert('click' + (index + 1))}
-              css={{ display: 'block' }}>
-              {index + 1}
-            </button>
-          ))}
-        </Main>
-      </div>
-    ),
-  ],
+  decorators: [reactRouterDecorator, (Story) => <Layout>{Story()}</Layout>],
   args: {
     items: ITEMS,
     renderLogo: <Logo />,
