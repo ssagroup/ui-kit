a47a6b71ed9ab2f5c115e1baae99a472c71286c9
Arthur <82376677+arthur-kashaiev-ssa-group@users.noreply.github.com>
2023-12-20 17:41:58 +0200

    <WithLink /> className (#151)

--- packages/widgets/src/components/AccountBalance/WithLink.tsx
+++ packages/widgets/src/components/AccountBalance/WithLink.tsx
@@ -6,13 +6,16 @@ export const WithLink = ({
   link,
   onClick,
   children,
+  className,
 }: Pick<AccountBalanceProps, 'link' | 'onClick'> & {
   children: React.ReactNode;
+  className?: string;
 }) =>
   link ? (
     <Link
       to={link}
       onClick={onClick}
+      className={className}
       css={css`
         text-decoration: none;
         div {
