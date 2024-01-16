9d7f84a244ccdc9898483523996d9ac5c879ec48
react-developer-ssa-group <141148643+react-developer-ssa-group@users.noreply.github.com>
2024-01-16 13:47:32 +0200

    Added forwardRef to the Textarea component (#159)

--- packages/core/src/components/Textarea/Textarea.tsx
+++ packages/core/src/components/Textarea/Textarea.tsx
@@ -1,37 +1,44 @@
+import React from 'react';
+import { useMergeRefs } from '@floating-ui/react';
 import { callAll } from '@ssa-ui-kit/utils';
-
 import { TextareaProps } from './types';
 import { TextareaBase } from './TextareaBase';
 
-const Textarea = ({
-  name,
-  placeholder,
-  register,
-  validationSchema,
-  disabled = false,
-  rows = 10,
-  setCountChar,
-  maxLength,
-  title,
-}: TextareaProps) => {
-  if (!register) {
-    throw new Error('Input component must be used within a Form component');
-  }
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  function TextareaInner(
+    {
+      name,
+      placeholder,
+      validationSchema,
+      disabled = false,
+      rows = 10,
+      maxLength,
+      title,
+      register,
+      setCountChar,
+    }: TextareaProps,
+    ref?: React.ForwardedRef<HTMLTextAreaElement | null>,
+  ) {
+    if (!register) {
+      throw new Error('Input component must be used within a Form component');
+    }
 
-  const { onChange, ...options } = register(name, validationSchema);
+    const { onChange, ...options } = register(name, validationSchema);
 
-  return (
-    <TextareaBase
-      id={`formElement-${name}`}
-      placeholder={placeholder}
-      disabled={disabled}
-      rows={rows}
-      maxLength={maxLength}
-      onChange={callAll(setCountChar, onChange)}
-      title={title}
-      {...options}
-    />
-  );
-};
+    return (
+      <TextareaBase
+        id={`formElement-${name}`}
+        placeholder={placeholder}
+        disabled={disabled}
+        rows={rows}
+        maxLength={maxLength}
+        onChange={callAll(setCountChar, onChange)}
+        title={title}
+        {...options}
+        ref={useMergeRefs([options.ref, ref])}
+      />
+    );
+  },
+);
 
 export default Textarea;
