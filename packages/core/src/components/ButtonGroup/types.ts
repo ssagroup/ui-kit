8cb67c918090d3574e8fcf488173c54cdf35772b
react-developer-ssa-group <141148643+react-developer-ssa-group@users.noreply.github.com>
2024-01-17 17:26:29 +0200

    Button group. External state (#161)

--- packages/core/src/components/ButtonGroup/types.ts
+++ packages/core/src/components/ButtonGroup/types.ts
@@ -9,6 +9,7 @@ export type ButtonGroupItem = {
 export interface ButtonGroupProps {
   items: Array<ButtonGroupItem>;
   selectedItem?: ButtonGroupItem;
+  externalState?: ButtonGroupItem;
   onClick: (item: ButtonGroupItem) => void;
   buttonStyles?: SerializedStyles;
 }
