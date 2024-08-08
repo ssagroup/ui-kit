53434710150d2a24da495aa60a902297327df34e
react-developer-ssa-group <141148643+react-developer-ssa-group@users.noreply.github.com>
2024-08-08 11:30:03 +0300

    CollapsibleNavBar logo changed (#197)
    close #196

--- packages/core/src/components/CollapsibleNavBar/stories/Logo.tsx
+++ packages/core/src/components/CollapsibleNavBar/stories/Logo.tsx
@@ -4,9 +4,9 @@ import * as S from './styles';
 export const Logo = () => (
   <ResponsiveImage
     css={S.ResponsiveLogo}
-    srcSet="https://firebasestorage.googleapis.com/v0/b/admin-themes.appspot.com/o/logo%2FCTP_Large_Left.png?alt=media&token=b6fe7ab8-fd0b-475f-bb08-360311f27693 69w, https://firebasestorage.googleapis.com/v0/b/admin-themes.appspot.com/o/logo%2FCTP_Medium_Left.png?alt=media&token=a1aeba69-7c07-40c9-aeac-c2477640870d 55w"
+    srcSet="https://firebasestorage.googleapis.com/v0/b/admin-themes.appspot.com/o/logo%2FUI_KIT_Large_Left_WhiteBG.png?alt=media&token=b6f7850c-2f1d-421c-a26f-f1f1280d676c 69w, https://firebasestorage.googleapis.com/v0/b/admin-themes.appspot.com/o/logo%2FUI_KIT_Medium_Left_WhiteBG.png?alt=media&token=27f3fcb9-c419-4e12-a18f-a16d362f54bf 55w"
     sizes="(min-width: 900px) 55px, (min-width: 1440px) 69px"
-    src="https://firebasestorage.googleapis.com/v0/b/admin-themes.appspot.com/o/logo%2FCTP_Small_Left.png?alt=media&token=bff7149e-3b90-4657-8a11-040e83990e6f"
-    alt="SSA CTP logo"
+    src="https://firebasestorage.googleapis.com/v0/b/admin-themes.appspot.com/o/logo%2FUI_KIT_Small_Left_WhiteBG.png?alt=media&token=c565fdb0-4ad9-42f5-8077-3249a5d727d8"
+    alt="SSA UI Kit"
   />
 );
