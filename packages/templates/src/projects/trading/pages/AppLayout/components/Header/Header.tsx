176d15ef8647726553c16725735f70082d26301d
Kostiantyn <kostiantyn.petrenko@ssa.group>
2024-10-08 13:56:27 +0300

    Cleaning up

--- packages/templates/src/projects/trading/pages/AppLayout/components/Header/Header.tsx
+++ packages/templates/src/projects/trading/pages/AppLayout/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import { HeaderBase, MenusSectionBase, PageSectionBase } from './HeaderBases';
-import { Breadcrumbs } from './Breadcrumbs';
 import { CustomContent } from './CustomContent';
 
 import { HeaderProps } from './types';
@@ -7,7 +6,6 @@ import { HeaderProps } from './types';
 export const Header = ({ children }: HeaderProps) => (
   <HeaderBase>
     <PageSectionBase>
-      <Breadcrumbs />
       <CustomContent />
     </PageSectionBase>
     <MenusSectionBase>{children}</MenusSectionBase>
