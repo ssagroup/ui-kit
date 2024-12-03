044ca7448e45f11d29bb96a3c3299d9331d670da
vadym-korzh-ssa-group <vadym.korzh@ssa.group>
2024-12-03 18:27:24 +0200

    #281 - Added two tests for CardHeader.

--- packages/core/src/components/CardHeader/CardHeader.spec.tsx
+++ packages/core/src/components/CardHeader/CardHeader.spec.tsx
@@ -2,11 +2,37 @@ import CardHeader from './CardHeader';
 import { screen } from '../../../customTest';
 
 describe('CardHeader', () => {
-  it('Render transparent card header', () => {
-    render(<CardHeader transparent>Card</CardHeader>);
+  it('Render initial background', () => {
+    render(<CardHeader>Card</CardHeader>);
 
     const cardHeader = screen.getByText(/card/i).closest('div');
 
+    expect(cardHeader).toHaveStyle('background: initial');
+  });
+});
+
+describe('CardHeader', () => {
+  it('Render initial background', () => {
+    const { getByText } = render(<CardHeader transparent>Card</CardHeader>);
+
+    const cardHeader = getByText(/card/i).closest('div');
+
     expect(cardHeader).toHaveStyle('background: transparent');
+  });
+});
+
+describe('CardHeader', () => {
+  it('Render with icon', () => {
+    const { getByText } = render(
+      <CardHeader icon={<div>Icon</div>}>Card</CardHeader>,
+    );
+
+    const div = getByText(/card/i).closest('div');
+    expect(div).not.toBeNull();
+
+    if (div !== null) {
+      const style = window.getComputedStyle(div);
+      expect(style.paddingLeft).toBe('30px');
+    }
   });
 });
