7cd49ab34884d260527fe9702885e17599c6e003
vadym-korzh-ssa-group <vadym.korzh@ssa.group>
2024-12-05 13:39:57 +0200

    #281 - Added tests for WithLink component.

---
+++ packages/core/src/components/WithLink.spec.tsx
@@ -0,0 +1,41 @@
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { WithLink } from './WithLink';
+
+describe('WithLink', () => {
+  it('Renders with link', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <Routes>
+          <Route path="/*" element={<WithLink link="/link">Child</WithLink>} />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    const linkEl = getByRole('link', {
+      name: new RegExp('Child', 'i'),
+    });
+
+    expect(linkEl).toHaveAttribute('href', '/link');
+  });
+
+  it('Renders with child element', () => {
+    const { getByTitle } = render(
+      <MemoryRouter>
+        <Routes>
+          <Route
+            path="/*"
+            element={
+              <WithLink>
+                <div title="Child"></div>
+              </WithLink>
+            }
+          />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    const div = getByTitle('Child');
+
+    expect(div).toBeInTheDocument();
+  });
+});
