a50efe2062e64c864b4d77e704fa4646536b6cc7
Dmytro Kushniruk <dmytro.kushniruk@ssa.group>
2025-02-25 17:01:50 +0200

    Update react-router-dom to version 7

--- packages/core/jest-setup.ts
+++ packages/core/jest-setup.ts
@@ -1,3 +1,8 @@
+import { TextEncoder } from 'node:util';
+
+// https://github.com/remix-run/react-router/issues/12363
+global.TextEncoder = TextEncoder;
+
 import '@testing-library/jest-dom';
 
 URL.createObjectURL = jest.fn();
