2a63fe82e976153c6264215c6f400d982a926444
Dmytro Kushniruk <dmytro.kushniruk@ssa.group>
2025-03-06 19:55:43 +0200

    Use type exports in Field and FiltersMultiSelect components

--- packages/core/src/components/Field/index.parts.ts
+++ packages/core/src/components/Field/index.parts.ts
@@ -2,5 +2,5 @@ export { FieldRoot as Root } from './FieldRoot';
 export { FieldError as Error } from './FieldError';
 export { FieldSuccess as Success } from './FieldSuccess';
 export { FieldDescription as Description } from './FieldDescription';
-export { FieldLabel as Label, FieldLabelProps } from './FieldLabel';
-export { FieldControl as Control, FieldControlProps } from './FieldControl';
+export { FieldLabel as Label } from './FieldLabel';
+export { FieldControl as Control } from './FieldControl';
