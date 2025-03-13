56ebff7108d92324734be77e07a5539e28a33335
react-developer-ssa-group <141148643+react-developer-ssa-group@users.noreply.github.com>
2025-03-13 09:53:18 +0200

    Nested table fixes (#367)

--- packages/core/src/components/NestedTable/types.ts
+++ packages/core/src/components/NestedTable/types.ts
@@ -1,5 +1,8 @@
+import { Dispatch, SetStateAction } from 'react';
+
 export type NestedTableRowContextType = {
   isCollapsed: boolean;
   isSubHeader: boolean;
   childRowsCount: number;
+  setIsCollapsed: Dispatch<SetStateAction<boolean>>;
 };
