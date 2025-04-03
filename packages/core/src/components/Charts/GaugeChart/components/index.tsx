da3e9d93f3e61eb6279788397b73e115830503a2
Dmytro Kushniruk <dmytro.kushniruk@ssa.group>
2025-04-03 21:32:22 +0300

    Add Gauge Chart

---
+++ packages/core/src/components/Charts/GaugeChart/components/index.tsx
@@ -0,0 +1,7 @@
+export * from './GaugeChartLabels';
+export * from './GaugeChartNeedle';
+export * from './GaugeChartTickers';
+export * from './GaugeChartBase';
+export * from './GaugeChartHeader';
+export * from './GaugeChartLayer';
+export * from './GaugeChartCanvas';
