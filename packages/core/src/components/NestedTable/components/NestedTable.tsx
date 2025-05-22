b9e9b08b01bc3b168042d96caff9771e4c6b5bc4
react-developer-ssa-group <141148643+react-developer-ssa-group@users.noreply.github.com>
2025-05-22 19:32:22 +0300

    NestedTable. Added some props (#459)

--- packages/core/src/components/NestedTable/components/NestedTable.tsx
+++ packages/core/src/components/NestedTable/components/NestedTable.tsx
@@ -1,15 +1,23 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, TableHTMLAttributes } from 'react';
+import { CommonProps } from '@global-types/emotion';
 import Table from '@components/Table';
 import { NestedTableProvider } from '../NestedTableContext';
 import { NestedTableContextType } from '../types';
 
 export const NestedTable = ({
   children,
+  collapsedIconName,
+  expandedIconName,
   ...rest
-}: PropsWithChildren & NestedTableContextType) => {
+}: PropsWithChildren &
+  NestedTableContextType &
+  CommonProps &
+  TableHTMLAttributes<HTMLTableElement>) => {
   return (
-    <NestedTableProvider {...rest}>
-      <Table>{children}</Table>
+    <NestedTableProvider
+      collapsedIconName={collapsedIconName}
+      expandedIconName={expandedIconName}>
+      <Table {...rest}>{children}</Table>
     </NestedTableProvider>
   );
 };
