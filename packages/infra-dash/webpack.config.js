04ab508937978dd40ea044bdd06bced4d6fe77b9
Dmytro Kushniruk <dmytro.kushniruk@ssa.group>
2025-06-11 11:36:37 +0300

    Add path aliases for infra-dash package

--- packages/infra-dash/webpack.config.js
+++ packages/infra-dash/webpack.config.js
@@ -5,6 +5,11 @@ module.exports = () => {
   const currentConfig = createConfig({
     libraryName: 'SSAInfraDash',
     outputPath: path.resolve(__dirname, 'dist'),
+    alias: {
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+    },
   });
 
   return currentConfig;
