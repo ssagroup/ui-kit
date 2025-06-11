be51d1a772b6dfaa04496274a30acbc2e4a380a2
Kostiantyn <kostiantyn.petrenko@ssa.group>
2025-06-11 17:58:30 +0300

    e2e test fixed

--- packages/core/src/components/Table/Table.e2e.ts
+++ packages/core/src/components/Table/Table.e2e.ts
@@ -2,7 +2,7 @@ import { test, expect, Page } from '@playwright/test';
 import { SCREEN_SIZES } from '../../consts';
 
 const CORE_CUSTOM_SHOTS_PATH = './custom-shots/';
-let SCREENSHOT_PREFIX = `${CORE_CUSTOM_SHOTS_PATH}widgets-bots-table--`;
+const SCREENSHOT_PREFIX = `${CORE_CUSTOM_SHOTS_PATH}widgets-bots-table--`;
 
 const gotoPage = (page: Page) => {
   return page.goto(
@@ -44,34 +44,6 @@ test.describe('Industry Specific Widgets - Training - BotsTable', () => {
 
     await page.screenshot({
       path: `${SCREENSHOT_PREFIX}run-reason-modal_[w1920px].png`,
-    });
-  });
-});
-
-const gotoPageNoControlOrders = (page: Page) => {
-  return page.goto(
-    'iframe.html?args=&id=components-table--no-control-orders&viewMode=story',
-  );
-};
-
-SCREENSHOT_PREFIX = `${CORE_CUSTOM_SHOTS_PATH}widgets-bots-table--no-control-orders--`;
-
-test.describe('Industry Specific Widgets - Training - BotsTable (NoControlOrders)', () => {
-  test('Renders correctly', async ({ page }) => {
-    await page.setViewportSize(SCREEN_SIZES[1920]);
-    await gotoPageNoControlOrders(page);
-    await expect(
-      page.locator('tr:nth-child(6)').getByText('Yevgen 2'),
-    ).toBeVisible();
-  });
-
-  test('Opens menu with Liquidate/Change Price/... items', async ({ page }) => {
-    await page.setViewportSize(SCREEN_SIZES[1920]);
-    await gotoPageNoControlOrders(page);
-    await page.locator('tr:nth-child(6) > td:nth-child(11) > button').click();
-
-    await page.screenshot({
-      path: `${SCREENSHOT_PREFIX}menu-opened_[w1920px].png`,
     });
   });
 });
