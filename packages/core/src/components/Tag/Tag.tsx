7d4cf5418907695772297a29d9e55d669a8a39b7
dmytro-vrodlyvets-ssa-group <dmytro.vrodlyvets@ssa.group>
2025-10-08 23:16:14 +0300

    feat: added custom styles support for Tag component

--- packages/core/src/components/Tag/Tag.tsx
+++ packages/core/src/components/Tag/Tag.tsx
@@ -1,4 +1,5 @@
 import Badge from '@components/Badge';
+import { css } from '@emotion/react';
 import {
   blue,
   green,
@@ -20,7 +21,7 @@ import {
   blueBorder,
   yellowWarmBorder,
 } from './styles';
-import { TagsProps } from './types';
+import { TagsProps, CustomTagStyles } from './types';
 
 const mapColors: MainColors = {
   pink: [pink, pinkBorder],
@@ -33,15 +34,62 @@ const mapColors: MainColors = {
   yellowWarm: [yellowWarm, yellowWarmBorder],
 };
 
+const createCustomStyles = (customStyles?: CustomTagStyles) => {
+  if (!customStyles) return null;
+
+  const styleProps = [];
+
+  if (customStyles.color) {
+    styleProps.push(css`
+      color: ${customStyles.color};
+    `);
+  }
+
+  if (customStyles.background) {
+    styleProps.push(css`
+      background: ${customStyles.background};
+    `);
+  }
+
+  if (customStyles.border) {
+    styleProps.push(css`
+      border: ${customStyles.border};
+    `);
+  }
+
+  if (customStyles.boxShadow) {
+    styleProps.push(css`
+      box-shadow: ${customStyles.boxShadow};
+    `);
+  }
+
+  if (customStyles.css) {
+    styleProps.push(customStyles.css);
+  }
+
+  return styleProps;
+};
+
 const Tag = ({
   color = 'purple',
   size = 'medium',
   className,
   children,
-}: TagsProps) => (
-  <Badge color={color} size={size} css={mapColors[color]} className={className}>
-    {children}
-  </Badge>
-);
+  customStyles,
+}: TagsProps) => {
+  const hasCustomStyles = customStyles && Object.keys(customStyles).length > 0;
+  const defaultStyles = hasCustomStyles ? null : mapColors[color];
+  const customStylesArray = createCustomStyles(customStyles);
+
+  return (
+    <Badge
+      color={hasCustomStyles ? 'transparent' : color}
+      size={size}
+      css={[defaultStyles, ...(customStylesArray || [])]}
+      className={className}>
+      {children}
+    </Badge>
+  );
+};
 
 export default Tag;
