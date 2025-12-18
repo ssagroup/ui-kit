321b668323ddcad77185d9b84770ce069a67f6eb
serhii.ukrainets <serhii.ukrainets@ssa.group>
2025-12-18 22:09:07 +0200

    wrapper for hoc

--- packages/core/src/components/Pagination/WithPagination.tsx
+++ packages/core/src/components/Pagination/WithPagination.tsx
@@ -1,16 +1,23 @@
+import React from 'react';
 import { PaginationContextProvider } from '.';
+import { setHocDisplayName } from '../../utils/react19HocCompat';
 
 export const WithPagination = <T extends object>(
   Component: React.ComponentType<T>,
-) => {
-  const decoratedComp = (props: T) => (
-    <PaginationContextProvider selectedPage={1}>
-      {/* TODO: HoC prop types not working with new emotion https://github.com/emotion-js/emotion/issues/3261 */}
-      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-      <Component {...(props as any)} />
-    </PaginationContextProvider>
-  );
+): React.ComponentType<T> => {
+  function WrappedComponent(props: T) {
+    return (
+      <PaginationContextProvider selectedPage={1}>
+        {/* TODO: HoC prop types not working with new emotion https://github.com/emotion-js/emotion/issues/3261 */}
+        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+        <Component {...(props as any)} />
+      </PaginationContextProvider>
+    );
+  }
 
-  decoratedComp.displayName = `WithPagination(${Component.displayName})`;
-  return decoratedComp;
+  return setHocDisplayName(
+    'WithPagination',
+    Component,
+    WrappedComponent,
+  ) as React.ComponentType<T>;
 };
