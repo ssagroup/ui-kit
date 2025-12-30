336b692b5b98feebbcc967eebe32d4e7f186fa84
serhii.ukrainets <serhii.ukrainets@ssa.group>
2025-12-30 18:57:37 +0200

    node 24?

--- scripts/get-node-options.js
+++ scripts/get-node-options.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
 const v = process.versions.node.split('.');
-const supportsNoWebstorage = v[0] >= 20 || (v[0] == 18 && v[1] >= 17);
+const major = parseInt(v[0]);
+
+// Node 24+ should definitely support --no-webstorage
+// Node 21-23 may have CI compatibility issues
+const supportsNoWebstorage = major >= 24;
+
+// Always include memory limit for large builds
+const baseOptions = ['--max_old_space_size=8192'];
 
 if (supportsNoWebstorage) {
-  console.log('--no-webstorage');
-} else {
-  console.log('');
+  baseOptions.push('--no-webstorage');
 }
+
+console.log(baseOptions.join(' '));
